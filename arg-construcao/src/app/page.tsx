d75ad523bca2d4539f9ed93e003463a4462c44fc
Pedro Pinto <1221499@isep.ipp.pt>
2025-02-06 18:25:58 +0000

    teste de deploy

--- arg-construcao/src/app/page.tsx
+++ arg-construcao/src/app/page.tsx
@@ -1,6 +1,7 @@
+// src/app/page.tsx
 import Head from 'next/head';
 import Image from 'next/image';
-import styles from '../styles/Home.module.css';
+import styles from './styles/Home.module.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
